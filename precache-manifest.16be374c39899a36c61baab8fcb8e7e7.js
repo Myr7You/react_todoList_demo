e42a173cbb7ccf3bdf61c3c93554177cbc948e9d
Myr7You <myr7you@gmail.com>
2020-04-07 15:44:10 +0800

    Updates

---
+++ precache-manifest.16be374c39899a36c61baab8fcb8e7e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af5e6adaddf82534af66534d373c6438",
+    "url": "/react_todoList_demo/index.html"
+  },
+  {
+    "revision": "d55116080f995af22eb6",
+    "url": "/react_todoList_demo/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e3ce72cd8d46303fcf18",
+    "url": "/react_todoList_demo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d55116080f995af22eb6",
+    "url": "/react_todoList_demo/static/js/2.b58ddf4d.chunk.js"
+  },
+  {
+    "revision": "b58d8a6c0e5872b60955ce6fbfbe23c2",
+    "url": "/react_todoList_demo/static/js/2.b58ddf4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3ce72cd8d46303fcf18",
+    "url": "/react_todoList_demo/static/js/main.c5689d2a.chunk.js"
+  },
+  {
+    "revision": "9a77cb00552fef8fac24",
+    "url": "/react_todoList_demo/static/js/runtime-main.e4a1de6a.js"
+  }
+]);
